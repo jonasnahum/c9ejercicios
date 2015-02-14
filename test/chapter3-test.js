5ea85406fdf960c2f9b2740a6d8560d41cd819a6
jonasnahum <jonasnahum@gmail.com>
2015-02-14 01:04:00 +0000

    prueba de travis 3

--- test/chapter3-test.js
+++ test/chapter3-test.js
@@ -3,7 +3,7 @@ const m = require("./../modules/chapter3.js");
 
 exports.countCharTest=function(test){
     test.expect(1);
-    let expected = 5;
+    let expected = 3;
     let actual = m.countChar("elefante","e");
     test.equal(actual,expected);
     test.done();
